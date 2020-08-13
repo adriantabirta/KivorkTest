96b532e9a72abe1108ccdf98e4dc1fd8699b1b0c
Adrian Tabirta <adriantabirta@gmail.com>
2020-08-13 15:10:33 +0300

    Initial Commit

---
+++ KivorkTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KivorkTest
+//
+//  Created by Adrian Tabirta on 8/13/20.
+//  Copyright © 2020 Adrian Tabirta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
